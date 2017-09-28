5c9f3f34f9bde1d6d0dec15464180dc3376d3eee
java <java@bigdata>
2017-09-28 12:39:34 +0900

    storyboard

--- src/main/java/com/example/controller/WebBoardController.java
+++ src/main/java/com/example/controller/WebBoardController.java
@@ -8,7 +8,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.domain.WebBoard;
 import com.example.persistence.WebBoardRepository;
@@ -25,6 +27,39 @@ public class WebBoardController {
 	
 	@Inject
 	WebBoardRepository repo;
+	
+	@GetMapping("/register")
+	public String registerGET(@ModelAttribute("vo")WebBoard vo ){
+		log.info("register get");
+		vo.setTitle("샘플 게시물 제목입니다....");
+		vo.setContent("내용을 처리해 봅니다 " );
+		vo.setWriter("user00");
+		
+		return "jsp/board/register";
+	}
+	
+	@PostMapping("/register")
+	public String registerPOST(@ModelAttribute("vo")WebBoard vo, RedirectAttributes rttr){
+		
+		log.info("register post");
+		log.info("" + vo);
+
+		repo.save(vo);
+		rttr.addFlashAttribute("msg", "success");
+		
+		return "redirect:/board/list";
+	}
+	
+	@GetMapping("/view")
+	public String view(Long bno, @ModelAttribute("pageVO") PageVO vo, Model model){
+		
+		log.info("BNO: "+ bno);
+		
+		repo.findById(bno).ifPresent(board -> model.addAttribute("vo", board));
+		
+		return "jsp/board/view";
+		
+	}
 	
 	@GetMapping("/list")
 	public String list(@ModelAttribute("pageVO") PageVO vo, Model model){
